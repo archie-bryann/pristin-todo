23b456712bfadbffb42d849a27b5ef7a7781a80e
Ekomobong Archibong <97162528+ekomobong-dev@users.noreply.github.com>
2022-01-12 15:39:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import { X } from "react-bootstrap-icons";
+import { v4 } from "uuid";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [date, setDate] = useState("");
+  const [note, setNote] = useState("");
+
+  const addNote = () => {
+    if (date == "" || note == "") {
+      alert("All fields are required!");
+    } else {
+      let _note = { id: v4(), note, date };
+      if (notes.length < 1) {
+        setNotes([_note]);
+      } else {
+        setNotes([...notes, _note]);
+      }
+      setNote("");
+      setDate("");
+    }
+  };
+
+  const deleteNote = (id) => {
+    const remainder = notes.filter((note) => {
+      if (note.id !== id) return note;
+    });
+    setNotes(remainder);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h3 align="center">Calender App</h3>
+      <input
+        type="text"
+        placeholder="Add a note"
+        value={note}
+        onChange={(e) => setNote(e.target.value)}
+      />
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <button onClick={addNote}>Add</button>
+      <div style={{ height: "15px" }}></div>
+      <hr />
+      <div className="notes">
+        {notes.map((item) => (
+          <div className="card-item" key={item.id}>
+            {item.note} - {item.date}
+            <X size={25} className="icon" onClick={() => deleteNote(item.id)} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
